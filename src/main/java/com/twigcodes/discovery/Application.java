9722ebb9b45eb6fe982519295cb2cdaebd2ae526
wpcfan <peng.wang@outlook.com>
2018-08-09 20:03:47 +0800

    twigcodes' discovery and admin server

---
+++ src/main/java/com/twigcodes/discovery/Application.java
@@ -0,0 +1,23 @@
+package com.twigcodes.discovery;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 服务发现服务器
+ */
+@SpringBootApplication
+@EnableAdminServer
+@EnableEurekaServer
+@RefreshScope
+@Configuration
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
